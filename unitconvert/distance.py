4f95fc75c251dfad463739ab2b19002e03fbc85b
elizabethcase <elizaebthhcase@gmail.com>
2017-11-16 09:31:04 -0500

    added universal function to distance and temp

--- unitconvert/distance.py
+++ unitconvert/distance.py
@@ -4,7 +4,7 @@ def miles_to_kilometers(distance):
         ---------
         distance : (float)
                     distance in miles
-                    
+
         RETURNS
         ---------
         distance : (float)
@@ -21,13 +21,34 @@ def kilometers_to_miles(distance):
         ---------
         distance : (float)
         	    distance in kilometers
-        
+
         RETURNS
         ---------
         distance : (float)
         	    distance in miles
         '''
-    
+
     miles = distance/1.61
-    
+
     return miles
+
+def dist_convert(distance, units=m):
+    '''
+        PARAMETERS
+        ---------
+        distance : (float)
+        	    distance in specified "units"
+        units : (string)
+                "m" for miles or "k" for kilometers
+
+        RETURNS
+        ---------
+        distance : (float)
+        	    distance in desired units
+    '''
+
+    if units=='m':
+        return miles_to_kilometers(distance)
+
+    if units=='k':
+        return kilometers_to_miles(distance)
--- unitconvert/temperature.py
+++ unitconvert/temperature.py
@@ -1,10 +1,12 @@
 def fahrenheit_to_celsius(temp):
     '''
         Input:
-            temp (float) - temp in fahrenheit
-            
+            temp: (float)
+                  - temp in fahrenheit
+
         Returns:
-            temp (float) - temp in celsius
+            temp: (float)
+                  - temp in celsius
     '''
     celsius = (32.0 - temp)*(5.0/9)
 
@@ -12,13 +14,33 @@ def fahrenheit_to_celsius(temp):
 
 def celsius_to_fahrenheit(temp):
     '''
-    Input:
-    temp (float) - temp in fahrenheit
-    
-    Returns:
-    temp (float) - temp in celsius
+        Input:
+            temp: (float)
+                  - temp in fahrenheit
+
+        Returns:
+            temp: (float)
+                  - temp in celsius
     '''
 
     fahrenheit = 32.0 + temp*9.0/5
 
     return fahrenheit
+
+def temp_convert(temp, units=c):
+    '''
+        Input:
+            temp: (float)
+                  - temp in units specified by units choice
+            units: (string)
+                  - units of temperature; "c" for celsius or "f" for fahrenheit
+
+        Returns:
+            temp: (float)
+                  - temp in units of desired conversion
+    '''
+    if units=="c":
+        return celsius_to_fahrenheit(temp)
+
+    if units=="f":
+        return fahrenheit_to_celsius(temp)
